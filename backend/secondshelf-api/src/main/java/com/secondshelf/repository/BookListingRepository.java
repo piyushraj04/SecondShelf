b27536e6ec5476048987812a2abf9833f6f2dfad
piyushraj04 <piyush004.raj@gmail.com>
2026-09-15 03:05:24 +0530

    done update api and tested

--- backend/secondshelf-api/src/main/java/com/secondshelf/repository/BookListingRepository.java
+++ backend/secondshelf-api/src/main/java/com/secondshelf/repository/BookListingRepository.java
@@ -3,6 +3,7 @@ package com.secondshelf.repository;
 import com.secondshelf.entity.BookListing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,8 +13,14 @@ import java.util.Optional;
 public interface BookListingRepository extends JpaRepository<BookListing,Long> {
 
     Optional<List<BookListing>> findBySellerId(Long sellerid);
-
-    @Query("select b from BookListing b where b.id =:listingId And b.seller.id =: sellerId")
-    Optional<BookListing> findByIdAndSellerId(Long listingId, Long sellerId);
+    @Query("""
+       SELECT b
+       FROM BookListing b
+       WHERE b.id = :listingId
+       AND b.seller.id = :sellerId
+       """)
+    Optional<BookListing> findByIdAndSellerId(
+            @Param("listingId") Long listingId,
+            @Param("sellerId") Long sellerId);
 
 }
